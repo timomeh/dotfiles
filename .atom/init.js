b0d5ad8eb5f7e71cef3bbefeeebf159f21e1dbb5
Timo Mämecke <timo@maemecke.com>
2019-03-26 23:41:05 +0100

    .atom_ui_changes

--- .atom/init.js
+++ .atom/init.js
@@ -1,6 +1,6 @@
 // Show random faces for "No matches found" in command palette,
 // each time the command palette opens.
-const NUM_OF_FACES = 16
+const NUM_OF_FACES = 30
 atom.commands.onDidDispatch(event => {
   if (event.type === 'command-palette:toggle') {
     const faceNames = Array.from(Array(NUM_OF_FACES)).map(
