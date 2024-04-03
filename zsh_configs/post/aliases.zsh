a1b31d770bab2cfb376704bf5cfe9fc0b2995596
Timo Mämecke <timo@maemecke.com>
2024-04-04 00:31:06 +0200

    Use zimfw/exa for exa aliases

--- zsh_configs/post/aliases.zsh
+++ zsh_configs/post/aliases.zsh
@@ -5,9 +5,5 @@ for al in `git wat`; do
   alias g${al%%=*}="git ${al%%=*}"
 done; IFS=$OLD_IFS
 
-alias el="exa -l"
-alias et="exa --tree --ignore-glob=node_modules"
-alias eg="exa -g"
-alias eo="exa -1"
 alias tailscale="/Applications/Tailscale.app/Contents/MacOS/Tailscale"
 alias pn="pnpm"
