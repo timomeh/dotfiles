51c9eb0ce422dca54e1d2fdcb85733c56923e692
Timo Mämecke <timo@maemecke.com>
2024-04-06 00:33:51 +0200

    Add comment

--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -3,6 +3,8 @@
 export LESS="-F -g -i -M -R -S -w -X -z-4"
 export THEFUCK_PRIORITY="git_hook_bypass=1100"
 export HOMEBREW_NO_ANALYTICS=1
+
+# Change directories in LS to underlined light purple
 export LS_COLORS=$LS_COLORS:'di=4;95:'
 
 eval "$(thefuck --alias)"
