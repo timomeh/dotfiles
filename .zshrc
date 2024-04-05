a8fb62d9188d626132b5338fa81a13b06f01e8fa
Timo Mämecke <timo@maemecke.com>
2024-04-06 00:22:48 +0200

    Move thefuck alias to tools

--- .zshrc
+++ .zshrc
@@ -1,2 +1,1 @@
 source "$HOME/dotfiles/zsh/zinit.zsh"
-eval $(thefuck --alias)
--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -4,3 +4,5 @@ export LESS="-F -g -i -M -R -S -w -X -z-4"
 export THEFUCK_PRIORITY="git_hook_bypass=1100"
 export HOMEBREW_NO_ANALYTICS=1
 export LS_COLORS=$LS_COLORS:'di=4;95:'
+
+eval "$(thefuck --alias)"
