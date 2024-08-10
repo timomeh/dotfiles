60169c221cc173910573dc5d3cfbbeecd7132f7e
Timo Mämecke <timo@maemecke.com>
2024-08-10 17:57:12 +0200

    Use zed as default editor

--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -3,6 +3,7 @@
 export LESS="-F -g -i -M -R -S -w -X -z-4"
 export THEFUCK_PRIORITY="git_hook_bypass=1100"
 export HOMEBREW_NO_ANALYTICS=1
+export EDITOR="zed --wait"
 
 # Change directories in LS to underlined light purple
 export LS_COLORS=$LS_COLORS:'di=4;95:'
