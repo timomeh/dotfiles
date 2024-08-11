0c933207744abdf39b875c8ea271c027589dc5dc
Timo Mämecke <timo@maemecke.com>
2024-08-11 15:18:04 +0200

    Switch from zed to vscode

--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -3,7 +3,7 @@
 export LESS="-F -g -i -M -R -S -w -X -z-4"
 export THEFUCK_PRIORITY="git_hook_bypass=1100"
 export HOMEBREW_NO_ANALYTICS=1
-export EDITOR="zed --wait"
+export EDITOR="code --wait"
 
 # Change directories in LS to underlined light purple
 export LS_COLORS=$LS_COLORS:'di=4;95:'
