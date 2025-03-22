50de7be2503b900e48047c6b8dcded3ba10d2a9e
Timo Mämecke <timo@maemecke.com>
2025-03-22 22:16:28 +0100

    Track more stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,9 +9,12 @@ fish_add_path ~/Library/pnpm
 
 set -gx MCFLY_FUZZY 2
 set -gx MCFLY_INTERFACE_VIEW TOP
-set -gx EDITOR "code --wait"
+set -gx VISUAL "code --wait"
 set -gx THEFUCK_PRIORITY "git_hook_bypass=1100"
 set -gx HOMEBREW_NO_ANALYTICS 1
+set -gx HOMEBREW_BUNDLE_FILE "~/.config/brew/brewfile"
+
+abbr -a bb brew bundle
 
 fzf_configure_bindings --history=\e\cr
 
