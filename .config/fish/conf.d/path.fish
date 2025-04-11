e3a93c33e07871a68cc2eadf891c72d8c03a24de
Timo Mämecke <timo@maemecke.com>
2025-04-11 21:45:52 +0200

    Add homebrew/sbin to path

--- .config/fish/conf.d/path.fish
+++ .config/fish/conf.d/path.fish
@@ -1,4 +1,5 @@
 fish_add_path ~/.local/bin
 fish_add_path /opt/homebrew/bin
+fish_add_path /opt/homebrew/sbin
 fish_add_path /Applications/Sublime\ Text.app/Contents/SharedSupport/bin
 fish_add_path ~/Library/pnpm
